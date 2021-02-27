5c0c53508f24f5af721ec538263e23fd53861e6d
manmosu <asobiyooou@yahoo.co.jp>
2021-02-27 14:41:17 +0900

    shareapp2/27

--- django_app/urls.py
+++ django_app/urls.py
@@ -21,8 +21,8 @@ from django.conf.urls.static import static  ###
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', include('hello.urls')),
-    path('sns/', include('sns.urls')), #☆
+    
+   
     path('share/', include('share.urls')), 
-    path('album/', include('album.urls')), ###
+   
 ] +  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
